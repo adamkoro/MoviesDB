10fa634dbb7dbfec7f5d41325151dbe734121b88
adamkoro <adam.koronics@gmail.com>
2024-01-07 21:55:55 +0100

    fix: add

--- movie_manager/alter/movies_const.sql
+++ movie_manager/alter/movies_const.sql
@@ -1,3 +1,9 @@
 ALTER TABLE movies
 ADD CONSTRAINT movies_pk PRIMARY KEY (id);
 /
+ALTER TABLE movies
+ADD CONSTRAINT fk_movies_genre_id FOREIGN KEY(genre_id) REFERENCES genres(id);
+/
+ALTER TABLE movies
+ADD CONSTRAINT fk_movies_director_id FOREIGN KEY(director_id) REFERENCES directors(id);
+/
