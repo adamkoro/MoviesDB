615051b853399ccbe7cd675b7a1b2c94676dff60
adamkoro <adam.koronics@gmail.com>
2024-01-08 19:48:09 +0100

    feat: delete movie

---
+++ movie_manager/script/test_delete_movie.sql
@@ -0,0 +1,16 @@
+DECLARE
+    v_movie_id movies.id%TYPE;
+    v_movie_title movies.title%TYPE := 'The Matrix';
+    v_role_id roles.id%TYPE;
+    v_rating_id ratings.id%TYPE;
+BEGIN
+    SELECT id INTO v_movie_id FROM movies WHERE title LIKE v_movie_title;
+    SELECT id INTO v_rating_id FROM ratings WHERE movie_id = v_movie_id;
+    SELECT id INTO v_role_id FROM roles WHERE movie_id = v_movie_id;
+    rating_management_pkg.delete_rating(v_rating_id);
+    role_management_pkg.delete_role(v_role_id);
+    movie_management_pkg.delete_movie(v_movie_id);
+END;
+/
+
+COMMIT;
