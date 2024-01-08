ea35f998d73386164508590e0182bfc90dc606c2
adamkoro <adam.koronics@gmail.com>
2024-01-08 15:06:45 +0100

    feat: ratings.rating value only can be between in 1 to 5

--- movie_manager/alter/ratings_const.sql
+++ movie_manager/alter/ratings_const.sql
@@ -1,3 +1,7 @@
 ALTER TABLE ratings 
 ADD CONSTRAINT fk_ratings_movie_id FOREIGN KEY(movie_id) REFERENCES movies(id);
 /
+
+ALTER TABLE ratings
+ADD CONSTRAINT rating_value_check CHECK (rating BETWEEN 1 AND 5);
+/
